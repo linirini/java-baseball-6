79c2e88f3d32bf99001d8450405506ec9d567bcd
linirini <2001yerin@naver.com>
2023-10-30 02:24:22 +0900

    feat : 기능 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,8 +3,10 @@ package baseball;
 import baseball.domain.Judgement;
 import baseball.domain.NumberGenerator;
 import baseball.domain.Referee;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * 객체 지향 프로그래밍
@@ -13,9 +15,28 @@ import java.util.List;
  *         3. 각 결과를 종합한다.
  */
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> computer = generator.createRandomNumber();
+
         Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(1,2,3),Arrays.asList(7,8,9));
-        System.out.println(result);
+
+        String result = "";
+        while(!result.equals("0 볼 3 스트라이크")){
+            result = referee.compare(computer, askNumbers());
+            System.out.println(result);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static List<Integer>askNumbers(){
+        System.out.print("숫자를 입력해 주세요 : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        List<Integer>numbers=new ArrayList<>();
+        for(String number : input.split("")){
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
     }
 }
