4a92eb1b92094ce4ca751ff2695a97a42d533b10
linirini <2001yerin@naver.com>
2023-12-11 19:39:39 +0900

    feat : implement game manager starting game

--- src/main/java/baseball/controller/GameManager.java
+++ src/main/java/baseball/controller/GameManager.java
@@ -1,9 +1,27 @@
 package baseball.controller;
 
+import baseball.domain.RandomNumbersGenerator;
+import baseball.domain.Referee;
+import baseball.view.InputView;
+import baseball.view.OutputEnum;
+import baseball.view.OutputView;
+
 public class GameManager {
 
-    public void startGame(){
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final RandomNumbersGenerator numbersGenerator = new RandomNumbersGenerator();
+    private final Referee referee = new Referee();
 
+    public void run(){
+        while(isRestart()){
+            startGame();
+            endGame();
+        }
+    }
+
+    public void startGame(){
+        outputView.printGameStartNotice();
     }
 
     public void endGame(){
