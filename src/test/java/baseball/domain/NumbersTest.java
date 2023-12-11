f46fcc791caf365af3e7c524c7f200916e81a906
linirini <2001yerin@naver.com>
2023-12-11 23:36:18 +0900

    test : refactor test into parameterized test by using csv source

--- src/test/java/baseball/domain/NumbersTest.java
+++ src/test/java/baseball/domain/NumbersTest.java
@@ -7,6 +7,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class NumbersTest {
 
@@ -37,16 +39,17 @@ public class NumbersTest {
     }
 
     @DisplayName("같은 숫자의 개수를 반환한다")
-    @Test
-    void count_same_numbers() {
+    @ParameterizedTest
+    @CsvSource({"3,2,1,3", "9,4,1,1", "4,5,6,0"})
+    void count_same_numbers(int number1, int number2, int number3, int count) {
         Numbers numbers = new Numbers(List.of(1, 2, 3));
-        assertThat(numbers.countSameNumbers(new Numbers(List.of(3, 2, 1)))).isEqualTo(3);
-        assertThat(numbers.countSameNumbers(new Numbers(List.of(9, 4, 1)))).isEqualTo(1);
-        assertThat(numbers.countSameNumbers(new Numbers(List.of(4, 5, 6)))).isEqualTo(0);
+        assertThat(numbers.countSameNumbers(
+                new Numbers(List.of(number1, number2, number3)))).isEqualTo(count);
     }
 
     @DisplayName("같은 숫자이면서 같은 위치에 있는 숫자의 개수를 반환한다")
-    @Test
+    @ParameterizedTest
+    @CsvSource({"3,2,1,1", "1,2,9,2", "4,5,6,0"})
     void count_same_numbers_on_same_position() {
         Numbers numbers = new Numbers(List.of(1, 2, 3));
         assertThat(numbers.countSameNumberOnSamePosition(new Numbers(List.of(3, 2, 1)))).isEqualTo(
