959868ae006f55a491204f4efa69c62ae5704322
linirini <2001yerin@naver.com>
2023-12-11 18:32:06 +0900

    feat : implement Game start notice output View

--- src/main/java/baseball/view/OutputEnum.java
+++ src/main/java/baseball/view/OutputEnum.java
@@ -1,7 +1,7 @@
 package baseball.view;
 
 public enum OutputEnum {
-    ;
+    GAME_START("숫자 야구 게임을 시작합니다.");
 
     private String message;
 
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,11 +1,13 @@
 package baseball.view;
 
+import static baseball.view.OutputEnum.GAME_START;
+
 import baseball.domain.GameResult;
 
 public class OutputView {
 
     public void printGameStartNotice(){
-
+        System.out.println(GAME_START.getMessage());
     }
 
     public void printGameResult(GameResult gameResult){
