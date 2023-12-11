91b5786cca2623a7e21d168a00e828db1a111d86
linirini <2001yerin@naver.com>
2023-12-11 18:49:35 +0900

    feat : implement creating random numbers

--- src/main/java/baseball/domain/RandomNumbersGenerator.java
+++ src/main/java/baseball/domain/RandomNumbersGenerator.java
@@ -1,4 +1,20 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomNumbersGenerator {
+
+    public Numbers createRandomNumbers(){
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return new Numbers(computerNumbers);
+    }
+
 }
