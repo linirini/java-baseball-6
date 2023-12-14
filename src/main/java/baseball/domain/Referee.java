51a0bc7aab21f91bb62a82c77020897a10064cdc
linirini <2001yerin@naver.com>
2023-12-14 16:45:30 +0900

    style : code convention applied

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -2,23 +2,24 @@ package baseball.domain;
 
 import java.util.List;
 
-public class Referee{
+public class Referee {
 
-    public String compare(List<Integer>computer,List<Integer>player){
+    public String compare(List<Integer> computer, List<Integer> player) {
         //몇개의 숫자가 같은지를 먼저 구한 뒤
         //스트라이크의 개수를 구해 뺀다.(= 볼의 개수)
         final Judgement judgement = new Judgement();
-        int correctCount = judgement.correctCount(computer,player);
+        int correctCount = judgement.correctCount(computer, player);
         int strike = 0;
-        for(int placeIndex=0;placeIndex<player.size();placeIndex++){
-            if(judgement.hasPlace(computer,placeIndex,player.get(placeIndex))){
+        for (int placeIndex = 0; placeIndex < player.size(); placeIndex++) {
+            if (judgement.hasPlace(computer, placeIndex, player.get(placeIndex))) {
                 strike++;
             }
         }
         int ball = correctCount - strike;
-        if(strike==0){
+        if (strike == 0) {
             return "낫싱";
         }
-        return ball + " 볼 "+strike + " 스트라이크";
+        return ball + " 볼 " + strike + " 스트라이크";
     }
+
 }
